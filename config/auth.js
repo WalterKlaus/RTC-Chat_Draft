67bb2cad98b27fe62fcc431ff218e79f81f8646d
WalterKlaus <info@walterklaus.de>
2018-11-08 17:26:27 +0100

    Add files via upload

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID': '416514945191813', // your App ID
-        'clientSecret': '047111129c8a7eb2918de24a0d87c906', // your App Secret
+        'clientID': 'xxxxxxxxxxxx', // your App ID
+        'clientSecret': 'xxxxxxxxxxxxxxxxxxxxxxxxx', // your App Secret
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'callbackURL' 	: 'https://walterklaus.de:61570/auth/facebook/callback'
     },
@@ -17,8 +17,8 @@ module.exports = {
     },
 
     'googleAuth' : {
-        'clientID': '659609766228-c3tq6s75n2vv388v2sdv2k67lekfqkvt.apps.googleusercontent.com',
-        'clientSecret': 'FbEfno73kMnecXkpLetJLeR7',
+        'clientID': '659609766228-xxxxxxxxxxxx.apps.googleusercontent.com',
+        'clientSecret': 'xxxxxxxxxxxxxxxxxxx',
         'callbackURL': 'https://walterklaus.de:61570/auth/google/callback'
     }
 
